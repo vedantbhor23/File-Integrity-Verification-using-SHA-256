41b62f0ed483f868aa1ed6f22a25aa19340423b7
Vedant Bhor <vedant23bhor@gmail.com>
2026-07-03 12:35:07 +0530

    changes-1

--- main.py
+++ main.py
@@ -5,3 +5,18 @@ import datetime
 import tkinter as tk
 from tkinter import filedialog, messagebox, scrolledtext
 
+
+# ------------------------------
+# 1. Calculate SHA-256 hash
+# ------------------------------
+def calculate_hash(file_path):
+    sha256 = hashlib.sha256()
+    try:
+        with open(file_path, "rb") as f:
+            while chunk := f.read(4096):
+                sha256.update(chunk)
+        return sha256.hexdigest()
+    except FileNotFoundError:
+        return None
+
+
